01fcee0788bbb4531dede32678540b82c71459d3
Teja <tejayerriboyina@gmail.com>
2025-06-12 13:14:03 +0530

    Updated SpecialOffers.jsx for responsiveness

--- src/sections/SpecialOffers.jsx
+++ src/sections/SpecialOffers.jsx
@@ -12,9 +12,9 @@ export const SpecialOffers = () => {
                 <h1 className="text-5xl text-red-400 font-bold">Special <span className="text-black">Offers</span></h1>
                 <p className="mt-8 text-gray-600 text-lg">Embark on a shopping journey that redefines your experience with unbeatable deals. From premier selections to incredible savings, we offer unparalleled value that sets us apart.</p>
                 <p className="mt-8 text-gray-600">Navigate a realm of possibilities designed to fulfill your unique desires, surpassing the loftiest expectations. Your journey with us is nothing short of exceptional</p>
-                <div className="mt-8 flex-1 flex items-center gap-5 justify-start">
-                    <button className="flex gap-3 px-10 py-3 bg-red-400 text-white rounded-4xl cursor-pointer">Shop now <img src={arrowRight} alt="" width={25}/></button>
-                    <button className="px-10 py-3 bg-white border rounded-4xl text-gray-600 cursor-pointer">Learn more</button>
+                <div className="mt-8 flex-1 flex items-center gap-5 justify-start max-sm:flex-col max-sm:justify-start max-sm:items-start">
+                    <button className="flex gap-3 px-10 py-3 bg-red-400 text-white rounded-full cursor-pointer">Shop now <img src={arrowRight} alt="" width={25}/></button>
+                    <button className="px-10 py-3 bg-white border rounded-full text-gray-600 cursor-pointer">Learn more</button>
                 </div>
             </div>
         </div>
